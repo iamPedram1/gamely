e5c09234044ac239a8576bbcaf57517be3edf0cd
iamPedram1 <pedram.pps757@gmail.com>
2025-11-14 15:41:27 +0330

    Change configs for deployment

--- src/core/utilities/configs.ts
+++ src/core/utilities/configs.ts
@@ -2,9 +2,14 @@ import config from 'config';
 import dotenv from 'dotenv';
 dotenv.config({ quiet: true });
 
+const NODE_ENV = process.env.NODE_ENV;
+
 // Configs
 export const appPort = Number(config.get('PORT'));
-export const appDbUrl = config.get('DB') as string;
+export const appDbUrl =
+  NODE_ENV === 'production'
+    ? process.env.MONGO_URI
+    : (config.get('DB') as string);
 export const apiVersion = Number(config.get('VERSION'));
 export const appUrl = `http://localhost:${appPort}`;
 export const prefixBaseUrl = (url: string) => `/api/v${apiVersion}${url}`;
